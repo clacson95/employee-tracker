0fe294511285ada2e6a161e9eb4ea4adc290a0e5
clacson95 <caleb.lacson@gmail.com>
2022-12-17 18:56:33 -0500

    added more dependencies, added connection

--- server.js
+++ server.js
@@ -1,4 +1,17 @@
+const express = require("express");
+const app = express();
 const mysql = require("mysql2");
 const inquirer = require("inquirer");
-require("console.table");
+const consoleTable = require("console.table");
+const PORT = process.env.PORT || 3306;
 
+const db = mysql.createConnection(
+    {
+        host: "localhost",
+        user: "root",
+        port: 3306,
+        password: "password",
+        database: "employee_db",
+    },
+    console.log("Successfully connected to the employee_db database.")
+);
