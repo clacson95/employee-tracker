06162b833780a847c9474676d6ac1173d3f3034b
clacson95 <caleb.lacson@gmail.com>
2022-12-17 19:09:53 -0500

    added inquirer prompts, and if/else if statements for calling associated functions

--- server.js
+++ server.js
@@ -15,3 +15,49 @@ const db = mysql.createConnection(
     },
     console.log("Successfully connected to the employee_db database.")
 );
+
+// function that runs the application, asking the user a series of prompts
+function runApp() {
+    inquirer.prompt([
+        {
+          type: "list",
+          name: "userChoice",
+          message: "What would you like to do? Please use the arrow keys to select an option, then hit Enter.",
+          choices: [
+            "View all departments",
+            "View all roles",
+            "View all employees",
+            "Add a department",
+            "Add a role",
+            "Add an employee",
+            "Update an employee role",
+            "Finish",
+          ],
+        },
+      ])
+      .then((response) => {
+       
+        console.log(response);
+
+        if (response.userChoice === "View all departments") {
+          viewDepartment();
+        } else if (response.userChoice === "View all roles") {
+          viewRoles();
+        } else if (response.userChoice === "View all employees") {
+          viewEmployees();
+        } else if (response.userChoice === "Add a department") {
+          addDepartment();
+        } else if (response.userChoice === "Add a role") {
+          addRole();
+        } else if (response.userChoice === "Add an employee") {
+          addEmployee();
+        } else if (response.userChoice === "Update an employee role") {
+          updateRole();
+        } else {
+          finish();
+        }
+      });
+  }
+  
+  // call the runApp() function to initialize the app
+  runApp();
